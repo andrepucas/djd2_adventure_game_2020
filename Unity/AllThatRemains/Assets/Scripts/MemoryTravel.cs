82719b8889d2226fe3da1dad9e4612478e18b0b2
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2020-12-13 19:00:21 +0000

    Added vault door animation and paper inventory icon

--- Unity/AllThatRemains/Assets/Scripts/MemoryTravel.cs
+++ Unity/AllThatRemains/Assets/Scripts/MemoryTravel.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
-
+using UnityEngine.Rendering;
 public class MemoryTravel : MonoBehaviour
+
 {
     private CharacterController _controller;
-    private Vector3             _distance;
-    private bool                _inMemoryTravel;
+    private Vector3 _distance;
+    private bool _inMemoryTravel;
+
+    private RenderPipelineAsset _UniversalRenderPipelineAsset;
+    //private RenderPipelineAsset _UniversalRenderPipelineAsset2;
 
     void Start()
     {
-        _controller     = GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
         _inMemoryTravel = false;
     }
 
     void Update()
     {
         CheckForMemoryTravel();
+
+        // if (Input.GetKeyDown(KeyCode.Space)) {
+        //     GraphicsSettings.renderPipelineAsset = 
+        //     _UniversalRenderPipelineAsset1;
+
+        // }
+        //     else if (Input.GetKeyDown(KeyCode.Space)) {
+        //     GraphicsSettings.renderPipelineAsset = 
+        //     _UniversalRenderPipelineAsset2;
+
+        // }
     }
 
     private void CheckForMemoryTravel()
@@ -23,11 +38,13 @@ public class MemoryTravel : MonoBehaviour
         {
             if (_inMemoryTravel)
             {
-                _distance = new Vector3(-15,0,0);
+                _distance = new Vector3(-15, 0, 0);
+
             }
             else
             {
-                _distance = new Vector3(15,0,0);
+                _distance = new Vector3(15, 0, 0);
+
             }
 
             Teleport();
@@ -36,11 +53,15 @@ public class MemoryTravel : MonoBehaviour
 
     private void Teleport()
     {
-            _controller.Move(transform.TransformVector(_distance));
-            
-            _inMemoryTravel = !_inMemoryTravel;
 
-            Debug.Log("Memory Travelled.");
-            Debug.Log($"MemoryTravel: {_inMemoryTravel}");
+        _controller.Move(transform.TransformVector(_distance));
+
+        _inMemoryTravel = !_inMemoryTravel;
+         GraphicsSettings.renderPipelineAsset = _UniversalRenderPipelineAsset;
+
+        Debug.Log("Memory Travelled.");
+        Debug.Log($"MemoryTravel: {_inMemoryTravel}");
+        Debug.Log("Active render pipeline asset is: " +
+        GraphicsSettings.renderPipelineAsset.name);
     }
 }
