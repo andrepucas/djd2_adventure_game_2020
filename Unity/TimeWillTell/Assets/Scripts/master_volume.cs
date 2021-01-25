79aab1c78b97911b7b68d894b907dce363a5d6bf
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2021-01-25 23:23:06 +0000

    Added Option Menu to both scenes and started connecting funcions to sliders

---
+++ Unity/TimeWillTell/Assets/Scripts/master_volume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class master_volume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void ChangeVolume (float volume)
+    {
+        audioMixer.SetFloat("master_volume", volume);
+    }
+}
