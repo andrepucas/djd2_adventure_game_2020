985b9f80e912afa8bb42ce33369704a46cf75a7c
andrepucas <andre.pucas.santos@gmail.com>
2021-01-26 02:49:39 +0000

    Black lampshade placed. Enables emission on itself and books

---
+++ Unity/TimeWillTell/Assets/Scripts/StatueControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueControl : MonoBehaviour
+{
+    [SerializeField] private Transform _leftForeArm;
+    [SerializeField] private Transform _leftHand;
+    [SerializeField] private Transform _rightForeArm;
+    [SerializeField] private Transform _rightHand;
+
+    private Quaternion _correctLeftForeArm;
+    private Quaternion _correctLeftHand;
+    private Quaternion _correctRightForeArm;
+    private Quaternion _correctRightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _correctLeftForeArm = new Quaternion (357.86499f,-2.66989588e-07f,230, 0);
+        _correctLeftHand = new Quaternion (9.86299801f,90,0.256000727f, 0);
+        _correctRightForeArm = new Quaternion (357.902191f,359.878326f,3.3159461f, 0);
+        _correctRightHand = new Quaternion (9.86799812f,180,359.884003f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_leftForeArm.rotation == _correctLeftForeArm &&
+            _leftHand.rotation == _correctLeftHand &&
+            _rightForeArm.rotation == _correctRightForeArm &&
+            _rightHand.rotation == _correctRightHand)
+
+        Debug.Log("Yes.");
+    }
+}
---
+++ Unity/TimeWillTell/Assets/Scripts/StudyLights.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudyLights : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer[] _lamp;
+    [SerializeField] private MeshRenderer[] _redBooks;
+    [SerializeField] private MeshRenderer[] _blueBooks;
+
+    [SerializeField] private Material _lampOriginal;
+    [SerializeField] private Material _redBooksOriginal;
+    [SerializeField] private Material _blueBooksOriginal;
+
+    [SerializeField] private Material _lampEmission;
+    [SerializeField] private Material _redBooksEmission;
+    [SerializeField] private Material _blueBooksEmission;
+
+    private Light _light;
+
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+    }
+
+    void Update()
+    {
+        if (_light.enabled)
+            EmissionOn();
+        
+        else
+            EmissionOff();
+    }
+
+    private void EmissionOn()
+    {
+        foreach (MeshRenderer lamp in _lamp)
+        {
+            lamp.material = _lampEmission;
+        }
+
+        foreach (MeshRenderer redBook in _redBooks)
+        {
+            redBook.material = _redBooksEmission;
+        }
+
+        foreach (MeshRenderer blueBook in _blueBooks)
+        {
+            blueBook.material = _blueBooksEmission;
+        }
+    }
+
+    private void EmissionOff()
+    {
+        foreach (MeshRenderer lamp in _lamp)
+        {
+            lamp.material = _lampOriginal;
+        }
+
+        foreach (MeshRenderer redBook in _redBooks)
+        {
+            redBook.material = _redBooksOriginal;
+        }
+
+        foreach (MeshRenderer blueBook in _blueBooks)
+        {
+            blueBook.material = _blueBooksOriginal;
+        }
+    }
+}
